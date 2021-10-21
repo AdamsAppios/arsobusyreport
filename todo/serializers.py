dab909323c2bc862d8ec2173fa46b3f5e04f500a
AdamsAppios <23veclipse23v@gmail.com>
2021-10-22 02:04:00 +0800

    hmm

--- todo/serializers.py
+++ todo/serializers.py
@@ -1,7 +1,16 @@
 from rest_framework import serializers
 from .models import Todo
 
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication 
+
+class CsrfExemptSessionAuthentication(SessionAuthentication):
+
+    def enforce_csrf(self, request):
+        return  # To not perform the csrf check previously happening
+
 class TodoSerializer(serializers.ModelSerializer):
+    authentication_classes = (CsrfExemptSessionAuthentication, BasicAuthentication)
+
     class Meta:
         model = Todo
         fields = ('id', 'title', 'description', 'completed')
--- todo/views.py
+++ todo/views.py
@@ -4,13 +4,6 @@ from .serializers import TodoSerializer
 from .models import Todo
 
 # Create your views here.
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication 
-
-class CsrfExemptSessionAuthentication(SessionAuthentication):
-
-    def enforce_csrf(self, request):
-        return  # To not perform the csrf check previously happening
-        
 class TodoView(viewsets.ModelViewSet):
     serializer_class = TodoSerializer
     queryset = Todo.objects.all()
