d56b6b6a9d47c86b636199027f238920dc7713e3
AdamsAppios <23veclipse23v@gmail.com>
2021-10-25 21:18:34 +0800

    Testina

--- todo/admin.py
+++ todo/admin.py
@@ -6,6 +6,6 @@ from .models import Todo
 class TodoAdmin(admin.ModelAdmin):
     list_display = ('title', 'description', 'completed')
 
-# Register your models here.
+# Register your models here. test
 
 admin.site.register(Todo, TodoAdmin)
