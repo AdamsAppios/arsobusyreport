5fe1cbc2388fc48f32c6d5aab8fd8c56a9456e97
Adam So <adamso@Adams-MacBook-Pro.local>
2023-06-16 18:36:47 +0800

    Second Commit

--- arsoreact1/src/components/MoneyCounter/Helperfunctions/Coinlargebut.js
+++
@@ -1,9 +0,0 @@
-const Coinlargebut = ({ handleIncrement, largeButton }) => {
-  return (
-    <div id="largestbuttonever" onClick={(e) => handleIncrement(1)}>
-      {largeButton}
-    </div>
-  );
-};
-
-export default Coinlargebut;
